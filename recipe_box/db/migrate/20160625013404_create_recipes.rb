4e24d1ae972686237ca39716e3757369070fd1b5
Ayush Jha <jha.ayush85@gmail.com>
2016-06-24 20:46:14 -0700

    Added CRUD to recipes

--- recipe_box/db/migrate/20160625013404_create_recipes.rb
+++ recipe_box/db/migrate/20160625013404_create_recipes.rb
@@ -1,6 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
+      t.string :title
+      t.text :description
+      t.integer :user_id
 
       t.timestamps null: false
     end
--- recipe_box/db/schema.rb
+++ recipe_box/db/schema.rb
@@ -11,14 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160625005449) do
+ActiveRecord::Schema.define(version: 20160625013404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "recipes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "widgets", force: :cascade do |t|
