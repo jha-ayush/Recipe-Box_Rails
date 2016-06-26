303a22e1b1e1651bba77527de5726d02f99b05de
Ayush Jha <jha.ayush85@gmail.com>
2016-06-26 13:16:57 -0700

    added styles

--- recipe_box/app/models/recipe.rb
+++ recipe_box/app/models/recipe.rb
@@ -1,2 +1,5 @@
 class Recipe < ActiveRecord::Base
+
+has_attached_file :image, styles: { :medium => "300x300#"}, default_url: "https://media.giphy.com/media/9Y6n9TR7U07ew/giphy.gif"
+validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
