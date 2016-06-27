352355b64851a267eb2fda5be1dadbfc3297c75d
Ayush Jha <jha.ayush85@gmail.com>
2016-06-27 02:06:07 -0700

    navbar

--- recipe_box/app/models/recipe.rb
+++ recipe_box/app/models/recipe.rb
@@ -1,5 +1,17 @@
 class Recipe < ActiveRecord::Base
 
+  has_many :ingredients
+	has_many :directions
+
+	accepts_nested_attributes_for :ingredients,
+  															reject_if: proc { |attributes| attributes['name'].blank? },
+  															allow_destroy: true
+ 	accepts_nested_attributes_for :directions,
+  															reject_if: proc { |attributes| attributes['step'].blank? },
+  															allow_destroy: true
+
+  validates :title, :description, :image, presence: true
+
 has_attached_file :image, styles: { :medium => "300x300#"}, default_url: "https://media.giphy.com/media/9Y6n9TR7U07ew/giphy.gif"
 validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
