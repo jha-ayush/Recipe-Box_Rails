17c892b9715106d649f36053d8ce1eb920af8dde
Ayush Jha <jha.ayush85@gmail.com>
2016-06-27 20:09:46 -0700

    authentication

--- recipe_box/app/models/recipe.rb
+++ recipe_box/app/models/recipe.rb
@@ -1,5 +1,7 @@
 class Recipe < ActiveRecord::Base
 
+  belongs_to :user
+
   has_many :ingredients
 	has_many :directions
 
@@ -15,4 +17,5 @@ class Recipe < ActiveRecord::Base
 
 has_attached_file :image, styles: { :medium => "300x300#"}, default_url: "https://media.giphy.com/media/9Y6n9TR7U07ew/giphy.gif"
 validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 end
